4e6fd7b40915d7dc62672bad6bc2e843770d03b0
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-11 20:17:33 -0600

    update composer

--- index.php
+++ index.php
@@ -1,37 +1,26 @@
- <?php
-//$hour = readline('Enter hour');
-$hour = 22;
-?>
+<?php 
 
+$fruits = ['apple', 'banana', 'orange', 'mango'];
+
+?>
 <!DOCTYPE html>
 
 <html>
-
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8" />
         <title>My Website</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="" />
     </head>
 
     <body>
         <header>
-        <h1>Lorem Ipsum</h1>
-</header>
-<main>
-        <?php if  ($hour < 12): ?>
-                Good Morning
-            <?php elseif ($hour < 18): ?>
-               Good Afternoon
-           <?php elseif ($hour < 22): ?>
-                Good Evening
-           <?php else : ?>
-               Good Night
-          <?php endif; ?>
-        <!-- <p>Hello, <?php echo $name  ?>!</p> -->
-           </main>
+            <h1>Lorem Ipsum</h1>
+        </header>
+        <main>
+           
+        </main>
     </body>
-            <footer>
-             </footer>
+    <footer></footer>
 </html>

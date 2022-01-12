e4e8b787e13d4d3cdd16dd391ab89ccf56b47b9c
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-12 11:15:54 -0600

    connected

--- index.php
+++ index.php
@@ -1,50 +1,14 @@
-<?php 
+<?php
 
-$articles = [
-[
-    'title' => 'First Post',
-    'content' => 'This is the first of many posts',
-],
-[
-    'title' => 'Another Post',
-    'content' => 'Yet another facinating  post'
-],
-[
-    "title" => "Read this!",
-    "content" => "You must read this now!",
-]
-];
-?>
-<!DOCTYPE html>
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "jerripat";
+$db_pass = "Dadio1005";
 
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>My Website</title>
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="" />
-    </head>
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
 
-    <body>
-        <header>
-            <h1>My Articles</h1>
-        </header>
-        <main>
-            <div>
-                <label> Fruit available today </label>
-                <ul>
-                    <?php  foreach($articles as $article): ?>
-                        <li>
-                            <article>
-                                <h2><?= $article['title']; ?></h2>    
-                                <p> <?= $article['content']; ?></p>
-                            </article>
-                    </li>
-                    <?php endforeach; ?>
-                    </ul>
-           </div>
-           
-        </main>
-    </body>
- </html>
+}
+echo "Connected Successfully";

05b116ac7f43d479699d8e846335d4da6696518b
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-16 10:25:22 -0600

    delete files drom diff project

---
+++ form.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+     <title>Forms</title>
+ </head>
+
+ <body>
+    
+     <form>
+            <input name="search">
+            
+            <button>Send</button>
+     
+     
+     </form>
+     
+    </body>
+ </html>

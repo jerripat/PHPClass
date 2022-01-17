1e3af85451e27ab6bfde117a106a7971c3a9a1e1
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-17 13:16:58 -0600

    Add New Article page

--- new-article.php
+++ new-article.php
@@ -1,17 +1,34 @@
-<?php require "includes/header.php" ?>
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {   
+        $sql = "SELECT * 
+                     FROM cms_article
+                     WHERE id = " . $_GET['id'];
+                    
+        $results = mysqli_query($conn, $sql);
 
 
+        if ($results === false) {
+            echo mysqli_error($conn);
+           
+        }
+        else {
+            
+            $article = mysqli_fetch_assoc($results);
+            
+        }
+    }
+?>
 
 <!doctype html>
 <head>
     <title>New Article</title>
-    
+    <link rel="stylesheet" type="text/css" href="CSS/transition.css"/>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/styles.css" />
     <style>
         form{
-            margin-top:3rem;
+            margin-top:1rem;
             margin-left:2rem;
         }
         label{
@@ -23,7 +40,7 @@
         }
         </style
     </head>
-    <form method="post">
+    <form method="post" class="item">
     <h2>New Article</h2>
  <div class="square">
     <div id="div1">
@@ -31,7 +48,7 @@
         <input name="title" id="title" placeholder="Article Title"> 
     </div>  
     
-    <div>
+    <div class=item>
             <label for="content">Content</label>
             <textarea name="content" rows="4" cols="40" id="content" placeholder="Article Content"></textarea>  
     </div>

cde00ce89c3ce8d310f97b051b0de381f207840e
jerripat <jerripatterson@gmail.comcore.editor>
2022-01-20 14:50:27 -0600

    updates

--- new-article.php
+++ new-article.php
@@ -1,43 +1,88 @@
 <?php 
 
 require 'includes/database.php';
-$errors = [];
 $title = ' ';
 $content = ' ';
 $published_at = ' ';
+$errors = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-        $conn = getID();
-         $sql = "INSERT INTO cms_article (title, content, published_at) VALUES (?, ?, ?)";
-            $errors[] = "Title is required";
-           
-            $stmt = mysqli_prepare($conn, $sql);
-          
-            if ($stmt === false) {
+    $title = $_POST[' title '];
+    $content = $_POST[ ' content '];
+    $published_at =$_POST[ 'published_at '];
+   
+    if ($title == '') {
+        $errors[] = 'Title is required';
+    }
+    if ($content == '') {
+        $errors[] = 'Content is required';
+    }
+
+    if ($published_at != ' ') {
+        $date_time = date_create_from_format('Y-m-d H:i:s', $published_at);
+        
+        if ($date_time === false) {
+            
+            $errors[] = 'Invalid date and time';
+            
+        } else {
+            
+            $date_errors = date_get_last_errors();
+            
+            if ($date_errors['warning_count'] > 0) {
+                $errors[] = 'Invalid date and time';
+            }
+        }
+    }
+            if (empty($errors)){
                 
-                echo mysqli_error($conn);
-            }    
-            else
-            {
-                mysqli_stmt_bind_param($stmt, "sss", $_POST[' title '], $_POST[' content '], $POST['published_at ']);
+                $conn = getDB();
+                
+                $sql = "INSERT INTO cms_article (title, content, published_at) VALUES (?, ?, ?)";
+                
+                $stmt = mysqli_prepare($conn, $sql);
+                
+                if ($stmt === false) {
+                    
+                    echo mysqli_error($conn);
+                }    
+                else
+                {
+                    if ($published_at == ' '){
+                        $published_at = null;
+                }
+
+                mysqli_stmt_bind_param($stmt, 'sss', $title, $content, $published_at);
                 
                 if  (mysqli_stmt_execute($stmt)){
-                    
-                    $id = mysqli_insert_id($conn);
-                    echo "Inserted record with ID: $id";
-            }   
-            else 
-            {
-                echo mysqli_stmt_error($stmt);
-           }    
+                
+                $id = mysqli_insert_id($conn);
+                
+                        if (isset($_SERVER[' HTTPS '] )  && $_SERVER[' HTTPS '] != ' off ' ){
+                                    $protocol = ' https ';
+                        }else {
+                                    $protocol = ' http ' ;
+                        }
+           
+                        header("Location: $protocol://" . $_SERVER[' HTTP_HOST '] . "/article.php?id=$id");
+                        exit;
+                    } else {
+                        echo mysqli_stmt_error($stmt);
+                        }   
+                    }
+            }       
         }
-   }
-
-?>
-
+ ?>
 <?php require 'includes/header.php'; ?>
-<h2> New Article</h2>
+<h2>New Article</h2>
+<?php if (! empty($errors)) : ?>
+        <ul>
+                <?php foreach ($errors as $error): ?>
+                    <li><?= $error ?></li>
+                <?php endforeach; ?>   
+        </ul> 
+<?php endif; ?>
 
 <form method="post">
 
